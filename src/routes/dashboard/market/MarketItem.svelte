eaedb2e91867a1c49bde1ac3a2703eb719994a40
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2025-12-23 22:13:00 +0000

    Change layout of market items

--- src/routes/dashboard/market/MarketItem.svelte
+++ src/routes/dashboard/market/MarketItem.svelte
@@ -17,12 +17,12 @@
 	<div class="aspect-square overflow-hidden rounded-lg bg-primary-800/10">
 		<img src={item.image} alt={item.name} class="h-full w-full object-contain object-center" />
 	</div>
-	<div class="flex flex-col gap-2 grow">
+	<div class="flex grow flex-col gap-2">
 		<div class="grow">
-			<h3 class="text-xl font-bold">{item.name}</h3>
-			<p class="mb-1 leading-snug text-primary-300">{item.description}</p>
+			<h3 class="text-center text-xl font-bold">{item.name}</h3>
+			<p class="mb-1 text-center leading-snug text-primary-300">{item.description}</p>
 			{#if item.discountAmount > 0}
-				<div class="flex items-center gap-2">
+				<div class="flex items-center justify-between gap-2">
 					<div class="text-lg text-primary-300 line-through">{item.maxPrice} bricks</div>
 					<div class="text-lg font-bold text-emerald-500">
 						{item.computedPrice} bricks
@@ -37,14 +37,16 @@
 					<div class="text-sm">You save {item.maxPrice - item.computedPrice} bricks</div>
 				</div>
 			{:else}
-				<div class="text-lg font-bold">{item.computedPrice} bricks</div>
+				<div class="text-center text-lg font-bold">{item.computedPrice} bricks</div>
 			{/if}
+		</div>
+		<div>
 			{#if item.minRequiredShopScore > userShopScore}
-				<div class="text-sm">
+				<div class="mb-1 text-sm text-center">
 					{item.minRequiredShopScore - userShopScore} more market score needed
 				</div>
 			{/if}
+			<a href={`market/${item.id}`} class={`button md primary`}>Buy</a>
 		</div>
-		<a href={`market/${item.id}`} class={`button md primary`}>Buy</a>
 	</div>
 </div>
