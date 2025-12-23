efcab5f78b763790d7dbbae7f52655ea3d041a2f
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2025-12-23 14:31:44 +0000

    Update hackatime page text

--- src/routes/auth/create-hackatime-account/+page.svelte
+++ src/routes/auth/create-hackatime-account/+page.svelte
@@ -7,7 +7,7 @@
 <div class="text-center mt-10">
     <h1 class="text-2xl">Hackatime account not found</h1>
     
-    <p>Make sure you have a Hackatime account associated with your Hack Club Auth or Slack account.</p>
+    <p>Make sure you have a Hackatime account associated with your Slack account.</p>
     <a href="https://hackatime.hackclub.com/" class="underline mr-3">Make one?</a>
     <a href="/auth/idv" class="underline">Try again</a>
 </div>
